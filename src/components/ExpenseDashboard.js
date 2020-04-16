35678de860cf44ba6e6ef94d99ed9d1f291c26dd
Zachary <53413395+ze-ck@users.noreply.github.com>
2020-04-16 12:39:47 -0400

    added visible expenses totalling component

--- src/components/ExpenseDashboard.js
+++ src/components/ExpenseDashboard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
 import ExpenseListFilters from './ExpenseListFilters';
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
     <div>
         <ExpenseList />
+        <ExpensesSummary />
         <ExpenseListFilters />
     </div>
 );
