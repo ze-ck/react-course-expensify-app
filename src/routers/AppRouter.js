c584cba559647d0c38682618be301b5aa359be96
Zachary <53413395+ze-ck@users.noreply.github.com>
2020-04-18 05:41:17 -0400

    add private routes and authentication

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
 import ExpenseDashboardPage from '../components/ExpenseDashboard';
 import AddExpense from '../components/AddExpense';
 import EditExpense from '../components/EditExpense';
-import Header from '../components/Header';
 import HelpPage from '../components/HelpPage';
 import NotFoundPage from '../components/NotFound';
+import LoginPage from '../components/LoginPage';
+import PrivateRoute from './PrivateRoute';
+
+export const history = createHistory();
 
 const AppRouter = () => (
-    <BrowserRouter>
+    <Router history={history}>
         <div>
-            <Header />
             <Switch>
-                <Route path="/" component={ExpenseDashboardPage} exact={true}/>
-                <Route path="/create" component={AddExpense} exact={true}/>
+                <Route path="/" component={LoginPage} exact={true} />
+                <PrivateRoute path="/dashboard" component={ExpenseDashboardPage} exact={true}/>
+                <PrivateRoute path="/create" component={AddExpense} exact={true}/>
+                <PrivateRoute path="/edit/:id" component={EditExpense} exact={true}/>
                 <Route path="/help" component={HelpPage} exact={true}/>
-                <Route path="/edit/:id" component={EditExpense} exact={true}/>
                 <Route component={NotFoundPage}/>
             </Switch> 
         </div>
-    </BrowserRouter>
+    </Router>
 );
 
 export default AppRouter;
